254de4160f26cf42fa0fe3fa64db6471110d07ce
ahmed emad <ahmed.3m4d@gmail.com>
2018-11-24 16:55:45 +0200

    end work with js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,90 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
+class Enemy {
+    constructor(x, y, speed) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed  = speed;
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for all computers.
+        this.x += this.speed * dt;
+        if (this.x >= 680) {
+            this.x = -85;
+        }
+        if (player.x < this.x + 80 && player.x + 80 > this.x && player.y < this.y + 60 && 60 + player.y > this.y) {
+            player.x = 300;
+            player.y = 500;
+        };
+    }
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/cat-girl.png';
+    }
 
+    update (x, y) {
+        if (this.y <= 0) {
+            this.x = 300;
+            this.y = 500;
+            gameEnd();      
+        }
+    }
+    // Draw the player on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    //to handel player move.
+    handleInput(moves) {
+        if (moves === 'up') {
+            this.update(this.y -= 85);
+        } else if (moves === 'down') {
+            this.update(this.y += 85);
+        } else if(moves === 'right') {
+            this.update(this.x += 100);
+        } else if(moves === 'left') {
+            this.update(this.x -= 100);
+        }
+    }
+}
+//to apper id won if game ended.
+function modal() {
+    const modal = document.querySelector('#won')
+    modal.classList.add('appear');    
+}
 
-// Now instantiate your objects.
+// Game end function if player won.
+function gameEnd() {
+    modal();
+    const playAgain = document.querySelector('.button');
+    playAgain.addEventListener('click', function() {
+        player.x = 300;
+        player.y = 500;
+    const modal = document.querySelector('#won');
+            modal.classList.remove('appear');   
+        });
+        
+}
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const enemyOne = new Enemy (0, 60, 600);
+const enemyTwo = new Enemy (0, 145, 500);
+const enemyThree = new Enemy (0, 230, 700);
+const enemyFour = new Enemy (0, 310, 600);
+const enemyFive = new Enemy (0,395, 900);
 
-
+let allEnemies = [enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive];
+let player = new Player(300, 500);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -24,10 +24,9 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    canvas.width = 707;
+    canvas.height = 670;
     doc.body.appendChild(canvas);
-
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
@@ -111,11 +110,12 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
-            numCols = 5,
+            numRows = 7,
+            numCols = 7,
             row, col;
         
         // Before drawing, clear existing canvas
@@ -173,7 +173,11 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/pink-girl.png',
+        'images/princess-girl.png',
+        'images/horn-girl.png',
+        'images/cat-girl.png'
     ]);
     Resources.onReady(init);
 
